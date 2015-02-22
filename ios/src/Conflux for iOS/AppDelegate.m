90877a4d14900d65e985b97a9402dd2c856ab354
Diego Pereira <diego.pereira@terra.com>
2015-02-22 18:07:28 -0300

    Renaming interface for compliance

--- ios/src/Conflux for iOS/AppDelegate.m
+++ ios/src/Conflux for iOS/AppDelegate.m
@@ -63,7 +63,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    self.s = [synergy new];
+    self.s = [Synergy new];
     [self.s loop];
 }
 
