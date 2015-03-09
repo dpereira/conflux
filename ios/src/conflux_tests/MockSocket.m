b78a3b877626373ad469d9a9dc347554dd457597
Diego Pereira <diego.pereira@terra.com>
2015-03-09 01:59:02 -0300

    Better mocks.
    Adding custom made mock that allows recording
    of commands to be sent to the code under test,
    as well as store the data sent to it from the
    code.

---
+++ ios/src/conflux_tests/MockSocket.m
@@ -0,0 +1,127 @@
+
+#import <Foundation/Foundation.h>
+#import "MockSocket.h"
+
+@interface CFXMockSocket()
+
+@end
+
+
+#define MAX_SENT_RECORDED 1024
+#define MAX_RECV_PLAYED_BACK 1024
+
+@implementation CFXMockSocket {
+    CFXParameters _sent[MAX_SENT_RECORDED];
+    CFXParameters _rcvd[MAX_RECV_PLAYED_BACK];
+    id<CFXSocketListener> _listener;
+    int _sentCount, _rcvdCount, _recordedCount;
+}
+
+- (id)init
+{
+    if(self = [super init]) {
+        self->_listener = nil;
+        self->_sentCount = 0;
+        self->_rcvdCount = 0;
+        memset(self->_sent, 0, sizeof(self->_sent));
+        memset(self->_rcvd, 0, sizeof(self->_sent));
+        return self;
+    } else {
+        return nil;
+    }
+}
+
+- (void) registerListener:(id<CFXSocketListener>)listener
+{
+    NSLog(@"Listener registered");
+    self->_listener = listener;
+}
+
+- (void)listen:(UInt16)port
+{}
+
+- (void)open
+{}
+
+- (size_t)saveTo:(CFXParameters*)storage
+          buffer:(const UInt8*)data
+           bytes:(size_t)howMany
+          offset:(int)index
+{
+    UInt8* bufferCopy = malloc(howMany);
+    memcpy(bufferCopy, data, howMany);
+    storage[index].buffer = bufferCopy;
+    storage[index].bytes = howMany;
+    return howMany;
+}
+
+-(void)recordWithHeader:(const UInt8 *)bytes bytes:(int)howMany {
+    UInt8 header[] = {0x00, 0x00, 0x00, howMany };
+    [self record:header bytes:sizeof(header)];
+    [self record:bytes bytes:howMany];
+}
+
+-(void)record:(const char *)payload {
+    NSLog(@"-> %s", payload);
+    [self recordWithHeader:(const UInt8 *)payload bytes:(int)strlen(payload)];
+}
+
+- (void)record:(const UInt8*)buffer
+         bytes:(size_t)howMany
+{
+    if(self->_recordedCount >= MAX_RECV_PLAYED_BACK) {
+        NSLog(@"Playback buffer is saturated");
+        return;
+    }
+    [self saveTo:self->_rcvd buffer:buffer bytes:howMany offset:self->_recordedCount];
+    self->_recordedCount += 1;
+}
+
+- (void)step
+{
+    NSLog(@"STEP");
+    [self->_listener receive:kCFXSocketReceivedData fromSender:self withPayload:NULL];
+}
+
+- (size_t)send:(const UInt8 *)buffer
+         bytes:(size_t)howMany
+{
+    if(self->_sentCount >= MAX_SENT_RECORDED) {
+        NSLog(@"Max send calls reached, dropping parameters.");
+        return howMany;
+    }
+    
+    [self saveTo:self->_sent buffer:buffer
+           bytes:howMany
+          offset:self->_sentCount];
+    
+    self->_sentCount += 1;
+    NSLog(@"Send interceptor %dx", self->_sentCount);
+
+    return howMany;
+}
+
+- (size_t)recv:(UInt8 *)buffer
+         bytes:(size_t)howMany
+{
+    NSLog(@"RECV called %lu in %dx", howMany, self->_rcvdCount);
+    if(self->_rcvdCount >= MAX_RECV_PLAYED_BACK ||
+       self->_rcvdCount >= self->_recordedCount)  {
+        NSLog(@"Max recv calls reached, dropping parameters.");
+        return 0;
+    }
+    
+    size_t bytes = self->_rcvd[self->_rcvdCount].bytes;
+    memcpy(buffer, self->_rcvd[self->_rcvdCount].buffer, bytes);
+    
+    self->_rcvdCount += 1;
+    
+    return bytes;
+}
+
+
+- (void)disconnect
+{
+}
+
+@end
--- ios/src/conflux_tests/SynergyTests.m
+++ ios/src/conflux_tests/SynergyTests.m
@@ -2,6 +2,7 @@
 #import <XCTest/XCTest.h>
 #import "OCMock.h"
 #import "Socket.h"
+#import "MockSocket.h"
 #import "Synergy.h"
 
 @interface CFXSynergy()
@@ -13,6 +14,44 @@
 @end
 
 @implementation SynergyTests
+
+// auxilliary
+
+- (void)_recordConnectionSequence:(CFXMockSocket*)recorder
+{
+
+    UInt8 hailResponseHeader[] = {
+        0x00, 0x00, 0x00, 0x17
+    };
+    UInt8 hailResponse[] = {
+        0x53, 0x79, 0x6e, 0x65,
+        0x72, 0x67, 0x79, 0x00,
+        0x01, 0x00, 0x05, 0x00,
+        0x00, 0x00, 0x08, 0x75,
+        0x6e, 0x69, 0x74, 0x5f,
+        0x64, 0x65, 0x65
+    };
+    [recorder record:hailResponseHeader bytes:sizeof(hailResponseHeader)];
+    [recorder record:hailResponse bytes:sizeof(hailResponse)];
+    
+    UInt8 dataResponseHeader[] = {
+        0x00, 0x00, 0x00, 0x12
+    };
+    UInt8 dataResponse[] = {
+        0x44, 0x49, 0x4e, 0x46,
+        0x00, 0x00, 0x00, 0x00,
+        0x05, 0x50, 0x03, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00
+    };
+    [recorder record:dataResponseHeader bytes:sizeof(dataResponseHeader)];
+    [recorder record:dataResponse bytes:sizeof(dataResponse)];
+    
+    [recorder record:"CALV"];
+    [recorder record:"CNOP"];
+}
+
+// tests
 
 - (void)testListening {
     id socket = OCMClassMock([CFXFoundationSocket class]);
@@ -33,11 +72,23 @@
     CFXPoint* resolution = [[CFXPoint alloc] initWith:320 and:240];
     [synergy load:resolution with:synergySocket];
     
-    id clientSocket = OCMClassMock([CFXFoundationSocket class]);
+    CFXMockSocket* clientSocket = [[CFXMockSocket alloc] init];
     
-    [synergy receive:kCFXSocketReceivedData fromSender:synergySocket withPayload:(__bridge void*)clientSocket];
+    [self _recordConnectionSequence:clientSocket];
     
-    OCMVerify([clientSocket send:[OCMArg anyPointer] bytes:(size_t)[OCMArg setToValue:OCMOCK_ANY]]);
+    [synergy receive:kCFXSocketConnected fromSender:synergySocket withPayload:(__bridge void*)clientSocket];
+    
+    [clientSocket step];
+    [clientSocket step];
+    [clientSocket step];
+    
+    [synergy beginMouseMove:[[CFXPoint alloc] initWith:20 and:20]];
+    
+    [clientSocket step];
+    
+    for(int i = 0; i < 100; i++) {
+        [synergy mouseMove:[[CFXPoint alloc ] initWith:i and:i]];
+    }
 }
 
 @end
