abdb35a9502ab016aef7a131143f875bf8d49595
Diego Pereira <diego@kaelor.local>
2015-04-28 00:53:38 -0300

    Improvements on layout; adding code to handle backspace (WiP)

--- ios/src/conflux/KeyboardViewController.m
+++ ios/src/conflux/KeyboardViewController.m
@@ -10,8 +10,11 @@
 #import "KeyboardViewController.h"
 #import "AppDelegate.h"
 #import "synergy/Synergy.h"
+@import UIKit;
 
 @interface KeyboardViewController ()
+
+@property NSString *_previousText;
 
 @end
 
@@ -19,11 +22,16 @@
 
 - (void)viewDidLoad
 {
+    [super viewDidLoad];
+    
+    self._previousText = @">";
     // FIXME: there must be a less
     // hackish way to accomplish this.
     UITextView *textArea = self.view.subviews[0];
     textArea.delegate = self;
     [textArea becomeFirstResponder];
+    textArea.text = self._previousText;
+    textArea.frame = [[UIScreen mainScreen] bounds];
 }
 
 - (void)textViewDidChange:(UITextView *)textView
@@ -32,6 +40,17 @@
     UInt16 c = [textArea.text characterAtIndex:textArea.text.length - 1];
     AppDelegate* app = [[UIApplication sharedApplication] delegate];
     [app._synergy keyStroke: c];
+}
+
+- (NSString*)_textAreaDelta
+{
+    UITextView *textArea = self.view.subviews[0];
+    NSString *currentText = textArea.text;
+    if(currentText.length > 1) {
+        return [currentText stringByReplacingOccurrencesOfString:self._previousText withString:@""];
+    } else {
+        return NULL;
+    }
 }
 
 @end
